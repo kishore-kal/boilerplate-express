f690b73bb72ae77764021639d9936c9d0ca8c364
Kishore Kumar <kmkishorekumar98@gmail.com>
2021-12-12 19:55:08 +0000

    add json response

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var app = express();
 
 const absolutePath = __dirname + '/views/index.html';
 
-
+const jsonObject = {"message": "Hello json"};
 console.log('Hello World');
 console.log(__dirname);
 // app.get('/',function(req,res){
@@ -14,7 +14,10 @@ app.use('/public',express.static(__dirname+'/public'));
 
 app.get('/',function(req,res){
   res.sendFile(absolutePath);
-  
+});
+
+app.get('/json',function(req,res){
+  res.json(jsonObject);
 })
 
 
