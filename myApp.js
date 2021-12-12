f7642f3bbd4c8469b5541bb4630c47ab0f685cc8
Kishore Kumar <kmkishorekumar98@gmail.com>
2021-12-12 19:51:07 +0000

    express static public

--- myApp.js
+++ myApp.js
@@ -3,15 +3,20 @@ var app = express();
 
 const absolutePath = __dirname + '/views/index.html';
 
-console.log('Hello World');
 
+console.log('Hello World');
+console.log(__dirname);
 // app.get('/',function(req,res){
 //   res.send('Hello Express');
 // })
 
+app.use('/public',express.static(__dirname+'/public'));
+
 app.get('/',function(req,res){
   res.sendFile(absolutePath);
+  
 })
+
 
 
 
