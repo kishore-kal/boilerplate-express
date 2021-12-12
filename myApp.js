6450c15034883d096a6171a88632a3106b1c0e1d
Kishore Kumar <kmkishorekumar98@gmail.com>
2021-12-12 18:57:35 +0000

    send Index file as response

--- myApp.js
+++ myApp.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
 
+const absolutePath = __dirname + '/views/index.html';
+
 console.log('Hello World');
 
+// app.get('/',function(req,res){
+//   res.send('Hello Express');
+// })
 
-
-
+app.get('/',function(req,res){
+  res.sendFile(absolutePath);
+})
 
 
 
